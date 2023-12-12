5af96d8168cd606fa0e6095890e8baab7417611c
Miras <miras.kenzhegaliyev@pipedrive.com>
2023-12-12 22:00:11 +0200

    Add white queen moves with some refactors

--- src/engine.js
+++ src/engine.js
@@ -139,42 +139,12 @@ function moveWhitePawn(chessBoard, toRankIndex, toFileIndex) {
  * @returns { {error?: string, result?: string[][]} }
  */
 function moveWhiteRook(chessBoard, toRankIndex, toFileIndex) {
-  let possibleRooks = [];
-  // rank up
-  for (let i = toRankIndex + 1; i < 8; i++) {
-    const piece = chessBoard[i][toFileIndex];
-    const location = [i, toFileIndex];
-    if (piece === empty) continue;
-    if (piece === white.Rook) possibleRooks.push(location);
-    break;
-  }
-
-  // rank down
-  for (let i = toRankIndex - 1; i >= 0; i--) {
-    const piece = chessBoard[i][toFileIndex];
-    const location = [i, toFileIndex];
-    if (piece === empty) continue;
-    if (piece === white.Rook) possibleRooks.push(location);
-    break;
-  }
-
-  // file up
-  for (let j = toFileIndex + 1; j < 8; j++) {
-    const piece = chessBoard[toRankIndex][j];
-    const location = [toRankIndex, j];
-    if (piece === empty) continue;
-    if (piece === white.Rook) possibleRooks.push(location);
-    break;
-  }
-
-  // file down
-  for (let j = toFileIndex - 1; j >= 0; j--) {
-    const piece = chessBoard[toRankIndex][j];
-    const location = [toRankIndex, j];
-    if (piece === empty) continue;
-    if (piece === white.Rook) possibleRooks.push(location);
-    break;
-  }
+  let possibleRooks = linearSearch(
+    chessBoard,
+    toRankIndex,
+    toFileIndex,
+    white.Rook
+  );
 
   if (possibleRooks.length === 1) {
     const loc = possibleRooks[0];
@@ -240,51 +210,12 @@ function moveWhiteKnight(chessBoard, toRankIndex, toFileIndex) {
  * @returns { {error?: string, result?: string[][]} }
  */
 function moveWhiteBishop(chessBoard, toRankIndex, toFileIndex) {
-  let possibleBishops = [];
-
-  for (let i = toRankIndex + 1, j = toFileIndex + 1; i < 8 && j < 8; i++, j++) {
-    const piece = chessBoard[i][j];
-    const location = [i, j];
-    if (piece === empty) continue;
-    if (piece === white.Bishop) possibleBishops.push(location);
-    break;
-  }
-
-  for (
-    let i = toRankIndex + 1, j = toFileIndex - 1;
-    i < 8 && j >= 0;
-    i++, j--
-  ) {
-    const piece = chessBoard[i][j];
-    const location = [i, j];
-    if (piece === empty) continue;
-    if (piece === white.Bishop) possibleBishops.push(location);
-    break;
-  }
-
-  for (
-    let i = toRankIndex - 1, j = toFileIndex + 1;
-    i >= 0 && j < 8;
-    i--, j++
-  ) {
-    const piece = chessBoard[i][j];
-    const location = [i, j];
-    if (piece === empty) continue;
-    if (piece === white.Bishop) possibleBishops.push(location);
-    break;
-  }
-
-  for (
-    let i = toRankIndex - 1, j = toFileIndex - 1;
-    i >= 0 && j >= 0;
-    i--, j--
-  ) {
-    const piece = chessBoard[i][j];
-    const location = [i, j];
-    if (piece === empty) continue;
-    if (piece === white.Bishop) possibleBishops.push(location);
-    break;
-  }
+  let possibleBishops = diagonalSearch(
+    chessBoard,
+    toRankIndex,
+    toFileIndex,
+    white.Bishop
+  );
 
   if (possibleBishops.length === 1) {
     const loc = possibleBishops[0];
@@ -298,6 +229,110 @@ function moveWhiteBishop(chessBoard, toRankIndex, toFileIndex) {
   } else {
     return {
       error: `${possibleBishops.length} bishops can move there`,
+    };
+  }
+}
+
+function linearSearch(chessBoard, rank, file, targetPiece) {
+  const foundPieces = [];
+  // rank up
+  for (let i = rank + 1; i < 8; i++) {
+    const piece = chessBoard[i][file];
+    const location = [i, file];
+    if (piece === empty) continue;
+    if (piece === targetPiece) foundPieces.push(location);
+    break;
+  }
+
+  // rank down
+  for (let i = rank - 1; i >= 0; i--) {
+    const piece = chessBoard[i][file];
+    const location = [i, file];
+    if (piece === empty) continue;
+    if (piece === targetPiece) foundPieces.push(location);
+    break;
+  }
+
+  // file up
+  for (let j = file + 1; j < 8; j++) {
+    const piece = chessBoard[rank][j];
+    const location = [rank, j];
+    if (piece === empty) continue;
+    if (piece === targetPiece) foundPieces.push(location);
+    break;
+  }
+
+  // file down
+  for (let j = file - 1; j >= 0; j--) {
+    const piece = chessBoard[rank][j];
+    const location = [rank, j];
+    if (piece === empty) continue;
+    if (piece === targetPiece) foundPieces.push(location);
+    break;
+  }
+  return foundPieces;
+}
+
+function diagonalSearch(chessBoard, rank, file, targetPiece) {
+  const possiblePieces = [];
+  for (let i = rank + 1, j = file + 1; i < 8 && j < 8; i++, j++) {
+    const piece = chessBoard[i][j];
+    const location = [i, j];
+    if (piece === empty) continue;
+    if (piece === targetPiece) possiblePieces.push(location);
+    break;
+  }
+
+  for (let i = rank + 1, j = file - 1; i < 8 && j >= 0; i++, j--) {
+    const piece = chessBoard[i][j];
+    const location = [i, j];
+    if (piece === empty) continue;
+    if (piece === targetPiece) possiblePieces.push(location);
+    break;
+  }
+
+  for (let i = rank - 1, j = file + 1; i >= 0 && j < 8; i--, j++) {
+    const piece = chessBoard[i][j];
+    const location = [i, j];
+    if (piece === empty) continue;
+    if (piece === targetPiece) possiblePieces.push(location);
+    break;
+  }
+
+  for (let i = rank - 1, j = file - 1; i >= 0 && j >= 0; i--, j--) {
+    const piece = chessBoard[i][j];
+    const location = [i, j];
+    if (piece === empty) continue;
+    if (piece === targetPiece) possiblePieces.push(location);
+    break;
+  }
+  return possiblePieces;
+}
+
+/**
+ *
+ * @param {string[][]} chessBoard
+ * @param {string} move
+ * @returns { {error?: string, result?: string[][]} }
+ */
+function moveWhiteQueen(chessBoard, toRankIndex, toFileIndex) {
+  let possibleQueens = [
+    ...linearSearch(chessBoard, toRankIndex, toFileIndex, white.Queen),
+    ...diagonalSearch(chessBoard, toRankIndex, toFileIndex, white.Queen),
+  ];
+
+  if (possibleQueens.length === 1) {
+    const loc = possibleQueens[0];
+    chessBoard[loc[0]][loc[1]] = empty;
+    chessBoard[toRankIndex][toFileIndex] = white.Queen;
+    return { result: chessBoard };
+  } else if (!possibleQueens.length) {
+    return {
+      error: `No queen can move there`,
+    };
+  } else {
+    return {
+      error: `${possibleQueens.length} queens can move there`,
     };
   }
 }
